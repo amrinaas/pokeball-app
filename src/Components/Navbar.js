43d893c7268f0c8e7db5a73cf8fb7d7031c88d9b
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-01-22 14:07:02 +0700

    Refactor navbar component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,162 +1,88 @@
-import React from 'react'
-import { NavLink } from "react-router-dom";
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 import Logo from '../Asset/image/pokeball.png'
-import { Link } from "react-router-dom";
-
-const pages = ['Collection', 'About'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Navbar = (props) => {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-    const [isLogin, setIsLogin] = React.useState(false);
-  
-    const handleOpenNavMenu = (event) => {
-      setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-      setAnchorElUser(event.currentTarget);
-    };
-  
-    const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-    };
-  
-    const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-    };
-  
-    return (
-      <div>
-        <AppBar position="static" color="transparent">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Link to={'/'}>
-                        <Avatar alt="logo" src={Logo} sx={{ 
-                            display: { xs: 'none', md: 'flex' }, 
-                            mr: 2 }} />
-                    </Link>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                            display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            {pages.map((page) => (
-                            <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                <Typography textAlign="center" sx={{ fontFamily: 'monospace' }}>
-                                    <NavLink to='/404'>{page}</NavLink>
-                                    </Typography>
-                            </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-                    <Link to={'/'}>
-                        <Avatar alt="logo" src={Logo} sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-                    </Link>
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href=""
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}>
-                        Pokemon
-                    </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button
-                            key={page}
-                            onClick={handleCloseNavMenu}
-                            sx={{ my: 2, color: 'white', display: 'block', fontFamily: 'monospace' }}
-                            >
-                            <NavLink to='/404'>{page}</NavLink>
-                            </Button>
-                        ))}
-                    </Box>
-                    <Box sx={{ flexGrow: 0 }}>
-                        { !isLogin ? 
-                            <NavLink to='/404'><Button variant="outlined">Login</Button></NavLink> : 
-                            <div>
-                                <Tooltip title="Open settings">
-                                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                                    </IconButton>
-                                </Tooltip>
-                                <Menu
-                                    sx={{ mt: '45px' }}
-                                    id="menu-appbar"
-                                    anchorEl={anchorElUser}
-                                    anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                    }}
-                                    keepMounted
-                                    transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                    }}
-                                    open={Boolean(anchorElUser)}
-                                    onClose={handleCloseUserMenu}>
-                                    {settings.map((setting) => (
-                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                        <Typography textAlign="center">{setting}</Typography>
-                                    </MenuItem>
-                                    ))}
-                                </Menu>
-                            </div>
-                        }
-                        
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
-        {props.children}
+  const [nav, setNav] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
+  const navItems = [
+    { id: 1, text: 'Home', url: '/' },
+    { id: 2, text: 'Collection', url: '/collection' },
+    { id: 3, text: 'About', url: '/about' },
+  ]
+
+  return (
+    <div className=''>
+      <div className='flex justify-between items-center h-24 mx-10 px-4 text-white'>
+        {/* Logo */}
+        <NavLink to={'/'}>
+          <div className='flex items-center justify-center'>
+            <img src={Logo} className='h-12 mr-2' alt='Logo' />
+            <h1 className='text-2xl font-bold'>PokeBall</h1>
+          </div>
+        </NavLink>
+
+        {/* Desktop Navigation */}
+        <ul className='hidden md:flex'>
+          {navItems.map((item) => (
+            <NavLink
+              to={item.url}
+              className={({ isActive }) =>
+                `p-4 hover:bg-[#f66947] rounded-xl m-2 cursor-pointer duration-300 hover:text-black ${
+                  isActive ? 'bg-[#f66947] text-black' : ''
+                }`
+              }
+              key={item.id}
+            >
+              <li>{item.text}</li>
+            </NavLink>
+          ))}
+        </ul>
+
+        {/* Mobile Navigation Icon */}
+        <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <span>Tutup</span> : <span>Buka</span>}
+        </div>
+
+        {/* Mobile Navigation Menu */}
+        <ul
+          className={
+            nav
+              ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+              : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+          }
+        >
+          {/* Mobile Logo */}
+          <NavLink to={'/'}>
+            <div className='flex items-center justify-center p-4'>
+              <img src={Logo} className='h-12 mr-2' alt='Logo' />
+              <h1 className='text-2xl font-bold'>PokeBall</h1>
+            </div>
+          </NavLink>
+
+          {/* Mobile Navigation Items */}
+          {navItems.map((item) => (
+            <NavLink
+              to={item.url}
+              className={({ isActive }) =>
+                `p-4 hover:bg-[#f66947] rounded-xl m-2 cursor-pointer duration-300 hover:text-black ${
+                  isActive ? 'bg-[#f66947] text-black' : ''
+                }`
+              }
+              key={item.id}
+            >
+              <li>{item.text}</li>
+            </NavLink>
+          ))}
+        </ul>
       </div>
-    );
+      {props.children}
+    </div>
+  )
 }
 
 export default Navbar
