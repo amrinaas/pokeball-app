d69fe628548fad15975da40c96c6848c31e15212
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-01-22 12:32:00 +0700

    Add loading spinner to card

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 const Cards = ({ image, name, id }) => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(false)
+  }, [id])
+
   return (
-    <Link to={`/pokemon/${id+1}`}>
-      <div key={id} className="card">
+    <Link to={`/pokemon/${id + 1}`}>
+      <div key={id} className='card'>
+        {loading && <div className='loader'></div>}
         <div>
-          <img src={`${image}/${id + 1}.png`} alt={name} />
+          <img
+            src={`${image}/${id + 1}.png`}
+            alt={name}
+            onLoad={() => setLoading(false)}
+            style={{ display: loading ? 'none' : 'block' }}
+          />
         </div>
-        <div style={{ fontSize: '20px', margin: 'auto' }}>
-          <b>{name.charAt(0).toUpperCase() + name.slice(1)}</b>
-        </div>
+        {!loading && (
+          <div style={{ fontSize: '20px', margin: 'auto' }}>
+            <b>{name.charAt(0).toUpperCase() + name.slice(1)}</b>
+          </div>
+        )}
       </div>
     </Link>
   )
