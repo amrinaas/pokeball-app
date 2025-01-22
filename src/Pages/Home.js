81f39bc2dd243f03599cceda8432bc9fddbd2ece
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-01-22 12:32:39 +0700

    Add infinite scroll

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,39 +1,73 @@
-import React, { useState, useEffect} from 'react'
-import { getPokemon } from '../Store/Action/Pokemon';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from 'react'
+import { getPokemon } from '../Store/Action/Pokemon'
+import { connect } from 'react-redux'
 
 import CardList from '../Components/CardList'
 import Navbar from '../Components/Navbar'
 
 const Home = (props) => {
-  const [limit, setLimit] = useState(50)
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
   const [offset, setOffset] = useState(0)
+  const limit = 20
 
-  React.useEffect(() => {
-		props.getPokemon({
-      limit: limit,
-      offset: offset
-    });
-	}, []);
+  useEffect(() => {
+    fetchPokemon()
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [offset])
 
-  return (
-      !props.pokemons.length ?
-        <h1>Loading...</h1> :
-        (
-          <Navbar>
-            <h1>Poké Card</h1>
-            <CardList pokemons={props.pokemons} />
-          </Navbar>
-        )
+  useEffect(() => {
+    setData((prev) => {
+      let updatedData = [...prev, ...props.pokemons].reduce((acc, current) => {
+        if (!acc.find((item) => item.name === current.name)) {
+          acc.push(current)
+        }
+        return acc
+      }, [])
+      return updatedData
+    })
+  }, [props.pokemons])
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      setOffset((prev) => prev + limit)
+    }
+  }
+
+  const fetchPokemon = async () => {
+    setLoading(true)
+    try {
+      await props.getPokemon({ limit, offset })
+    } catch (error) {
+      console.error('Error fetching pokemon', error)
+    } finally {
+      setTimeout(() => {
+        setLoading(false)
+      }, 3000)
+    }
+  }
+
+  return data.length === 0 && loading ? (
+    <h1>Loading</h1>
+  ) : (
+    <Navbar>
+      <h1>Poké Card</h1>
+      <CardList pokemons={data} />
+      {loading && <h1>Loading more...</h1>}
+    </Navbar>
   )
 }
 
 const mapStateToProps = (state) => {
   return {
-      pokemons: state.Pokemon.pokemons
+    pokemons: state.Pokemon.pokemons,
   }
 }
 
 export default connect(mapStateToProps, {
-  getPokemon
+  getPokemon,
 })(Home)
