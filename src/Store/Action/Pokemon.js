0af823c1b7f63ac28bf00ff95b4c82629609a015
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-01-23 09:12:54 +0700

    Create species and clear pokemon action and reducer

--- src/Store/Action/Pokemon.js
+++ src/Store/Action/Pokemon.js
@@ -1,27 +1,54 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const getPokemon = (params) => (dispatch) => {
-    const { limit, offset } = params;
-    axios
+  const { limit, offset } = params
+  axios
     .get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
     .then((response) => {
-        dispatch({
-            type: "GET_POKEMON",
-            payload: response.data.results
-        });
+      dispatch({
+        type: 'GET_POKEMON',
+        payload: response.data.results,
+      })
     })
-    .catch((err) => console.log('error', err));
+    .catch((err) => console.log('error', err))
 }
 
 export const detailPokemon = (params) => (dispatch) => {
-    const { id } = params;
-    axios
+  const { id } = params
+  axios
     .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then((response) => {
-        dispatch({
-            type: "DETAIL_POKEMON",
-            payload: response.data
-        });
+      dispatch({
+        type: 'DETAIL_POKEMON',
+        payload: response.data,
+      })
     })
-    .catch((err) => console.log('detail error', err));
+    .catch((err) => console.log('detail error', err))
+}
+
+export const pokemonSpecies = (params) => (dispatch) => {
+  const { id } = params
+  axios
+    .get(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
+    .then((response) => {
+      dispatch({
+        type: 'POKEMON_SPECIES',
+        payload: response.data,
+      })
+      return axios.get(response.data.evolution_chain.url)
+    })
+    .then((response) => {
+      console.log('evolution', response.data)
+      dispatch({
+        type: 'EVOLUTION_CHAIN',
+        payload: response.data,
+      })
+    })
+    .catch((err) => console.log('detail error', err))
+}
+
+export const clearPokemon = () => (dispatch) => {
+  dispatch({
+    type: 'CLEAR_POKEMON',
+  })
 }
--- src/Store/Reducer/Pokemon.js
+++ src/Store/Reducer/Pokemon.js
@@ -1,22 +1,41 @@
 const initialState = {
-    pokemons: [],
-    detail: null
-};
+  pokemons: [],
+  detail: null,
+  species: null,
+  evolutionChain: null,
+}
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case 'GET_POKEMON':
-            return {
-                ...state,
-                pokemons: action.payload
-            }
-        case 'DETAIL_POKEMON':
-            return {
-                ...state,
-                detail: action.payload
-            }
-        default:
-            break;
-    }
-    return state;
+  switch (action.type) {
+    case 'GET_POKEMON':
+      return {
+        ...state,
+        pokemons: action.payload,
+      }
+    case 'DETAIL_POKEMON':
+      return {
+        ...state,
+        detail: action.payload,
+      }
+    case 'POKEMON_SPECIES':
+      return {
+        ...state,
+        species: action.payload,
+      }
+    case 'EVOLUTION_CHAIN':
+      return {
+        ...state,
+        evolutionChain: action.payload,
+      }
+    case 'CLEAR_POKEMON':
+      return {
+        ...state,
+        detail: null,
+        species: null,
+        evolutionChain: null,
+      }
+    default:
+      break
+  }
+  return state
 }
