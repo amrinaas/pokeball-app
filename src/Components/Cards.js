4883444b4b2fa66631adebdbf7970ef2d3e21aaf
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-02-11 09:02:32 +0700

    Change card style

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Loading from './Loading'
+import Badge from './Badge'
 
-const Cards = ({ image, name, id, color, stats, type }) => {
+const Cards = ({ image, name, id, color, type }) => {
   const [loading, setLoading] = useState(true)
 
   const getColorHex = (color) => {
@@ -21,6 +22,29 @@ const Cards = ({ image, name, id, color, stats, type }) => {
     return `${colors[color]}66` || '#CCCCC'
   }
 
+  const getColorBadgeHex = (type) => {
+    const colors = {
+      grass: '#008000', // green
+      fire: '#FF0000', // red
+      water: '#0000FF', // blue
+      electric: '#FFAE42', // yellow orange
+      ice: '#ADD8E6', // light blue
+      fighting: '#A52A2A', // brown
+      poison: '#800080', // purple
+      ground: '#D2B48C', // tan
+      flying: '#B0E0E6', // light sky blue
+      psychic: '#FF69B4', // hot pink
+      bug: '#9ACD32', // yellow green
+      rock: '#808080', // gray
+      ghost: '#4B0082', // indigo
+      dragon: '#000080', // navy
+      dark: '#000000', // black
+      steel: '#C0C0C0', // silver
+      fairy: '#FFC0CB', // pink
+    }
+    return colors[type] ? `${colors[type]}` : '#808080'
+  }
+
   useEffect(() => {
     setLoading(false)
   }, [id])
@@ -29,8 +53,8 @@ const Cards = ({ image, name, id, color, stats, type }) => {
     <Link to={`/pokemon/${id}`}>
       <div
         key={id}
-        style={{ backgroundColor: getColorHex(color) }}
-        className={`shadow-xl text-black ease-out transition-all duration-300 m-3 p-1 rounded-md flex flex-col justify-center items-center hover:scale-110 hover:cursor-pointer hover:shadow-2xl`}
+        style={{ borderColor: getColorHex(color) }}
+        className={`shadow-xl border-4 bg-white text-black ease-out transition-all duration-300 m-3 p-1 rounded-lg flex flex-col justify-center items-center hover:scale-110 hover:cursor-pointer hover:shadow-2xl`}
       >
         {loading && <Loading />}
         {!loading && (
@@ -40,30 +64,24 @@ const Cards = ({ image, name, id, color, stats, type }) => {
               {name.charAt(0).toUpperCase() + name.slice(1)}
             </h5>
             <img
-              src={`${image}/${id}.png`}
+              src={`${image}`}
               alt={name}
               onLoad={() => setLoading(false)}
               className={`h-32 w-32 object-contain ${
                 loading ? 'hidden' : 'block'
               }`}
             />
-            <p className='text-sm'>Type: {type}</p>
-            <div className='p-2 w-full text-xs'>
-              {stats.map((stat, i) => (
-                <div key={i} className='flex px-1 items-center'>
-                  <p className='w-1/3 text-left'>{stat.stat.name} </p> :{' '}
-                  <div className='bg-gray-200 w-3/5 h-2 rounded-full overflow-hidden'>
-                    <div
-                      className='bg-green-500 h-2 w-full'
-                      style={{ width: `${stat.base_stat}%` }}
-                    ></div>
-                  </div>
-                  <p className='w-1/6'>{stat.base_stat}</p>
-                </div>
-              ))}
-            </div>
           </>
         )}
+        <div className='flex flex-wrap mb-3 justify-around mt-5 w-full'>
+          {type.map((t, i) => (
+            <Badge
+              key={i}
+              text={t.type.name}
+              color={getColorBadgeHex(t.type.name)}
+            />
+          ))}
+        </div>
       </div>
     </Link>
   )
