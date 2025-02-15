0ceeb8c635b00c4045e026192d21b92691a1df24
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-02-15 13:34:30 +0700

    Add footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='p-4 mt-3 text-xs text-right bottom-0'>
+      © 2023 Pokédex
+    </footer>
+  )
+}
+
+export default Footer
