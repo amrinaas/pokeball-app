e740d60928dd22dfe1a4db61af8b8269bb6ae265
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-02-15 13:34:38 +0700

    Change style for badge

--- src/Components/Badge.js
+++ src/Components/Badge.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Badge = ({ text, color }) => {
   return (
-    <div
-      className='px-5 py-1 text-xs border-slate-400/50 mb-1 border-[1px] rounded-xl tracking-wider shadow-2xl text-white capitalize'
+    <span
+      className='px-5 py-1 text-xs border-slate-400/50 mb-1 border-[1px] rounded-md tracking-wider shadow-2xl cursor-pointer text-white'
       style={{ backgroundColor: color }}
     >
       {text}
-    </div>
+    </span>
   )
 }
 
