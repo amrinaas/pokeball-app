dc0db6c45d2cf0b42d7466ef0700d2b9661c27fe
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-02-15 13:34:44 +0700

    Add footer component

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 const Layout = ({ children }) => {
   return (
     <div className='w-full min-h-screen bg-gray-200/50 overflow-x-hidden'>
       <Navbar />
       {children}
+      <Footer />
     </div>
   )
 }
