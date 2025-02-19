63bae192d4233e5600ff5cd9d902ad8d272c1be8
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-02-19 10:09:00 +0700

    Add catch pokemon icon

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -2,48 +2,11 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Loading from './Loading'
 import Badge from './Badge'
+import { getColorHex, getColorBadgeHex } from '../utils'
+import CatchIcon from '../Asset/image/catch.svg'
 
 const Cards = ({ image, name, id, color, type }) => {
   const [loading, setLoading] = useState(true)
-
-  const getColorHex = (color) => {
-    const colors = {
-      black: '#000000',
-      blue: '#0000FF',
-      brown: '#A52A2A',
-      gray: '#808080',
-      green: '#008000',
-      pink: '#FFC0CB',
-      purple: '#800080',
-      red: '#FF0000',
-      white: '#CCCCCC',
-      yellow: '#FFFF00',
-    }
-    return `${colors[color]}66` || '#CCCCC'
-  }
-
-  const getColorBadgeHex = (type) => {
-    const colors = {
-      grass: '#008000', // green
-      fire: '#FF0000', // red
-      water: '#0000FF', // blue
-      electric: '#FFAE42', // yellow orange
-      ice: '#ADD8E6', // light blue
-      fighting: '#A52A2A', // brown
-      poison: '#800080', // purple
-      ground: '#D2B48C', // tan
-      flying: '#B0E0E6', // light sky blue
-      psychic: '#FF69B4', // hot pink
-      bug: '#9ACD32', // yellow green
-      rock: '#808080', // gray
-      ghost: '#4B0082', // indigo
-      dragon: '#000080', // navy
-      dark: '#000000', // black
-      steel: '#C0C0C0', // silver
-      fairy: '#FFC0CB', // pink
-    }
-    return colors[type] ? `${colors[type]}` : '#808080'
-  }
 
   useEffect(() => {
     setLoading(false)
@@ -54,11 +17,20 @@ const Cards = ({ image, name, id, color, type }) => {
       <div
         key={id}
         style={{ borderColor: getColorHex(color) }}
-        className={`shadow-xl border-4 bg-white text-black ease-out transition-all duration-300 m-3 p-1 rounded-lg flex flex-col justify-center items-center hover:scale-110 hover:cursor-pointer hover:shadow-2xl`}
+        className={`shadow-xl border-4 bg-white text-black ease-out transition-all duration-300 m-3 p-1 rounded-lg flex flex-col justify-center items-center hover:scale-110 hover:cursor-pointer hover:shadow-2xl relative`}
       >
         {loading && <Loading />}
         {!loading && (
           <>
+            <div className='absolute top-0 left-0 w-16 h-16'>
+              <div className='w-10 h-10 bg-green-600/80 rounded-ee-md hover:bg-orange-400/80 transition-colors '>
+                <img
+                  src={CatchIcon}
+                  alt='catch-ball'
+                  className='w-5 h-5 absolute top-2 left-2'
+                />
+              </div>
+            </div>
             <h6 className='text-right w-full font-bold'>#{id}</h6>
             <h5 className='text-xl font-semibold'>
               {name.charAt(0).toUpperCase() + name.slice(1)}
---
+++ src/utils.js
@@ -0,0 +1,38 @@
+export const getColorHex = (color) => {
+  const colors = {
+    black: '#000000',
+    blue: '#0000FF',
+    brown: '#A52A2A',
+    gray: '#808080',
+    green: '#008000',
+    pink: '#FFC0CB',
+    purple: '#800080',
+    red: '#FF0000',
+    white: '#CCCCCC',
+    yellow: '#FFFF00',
+  }
+  return `${colors[color]}66` || '#CCCCCC'
+}
+
+export const getColorBadgeHex = (type) => {
+  const colors = {
+    grass: '#008000', // green
+    fire: '#FF0000', // red
+    water: '#0000FF', // blue
+    electric: '#FFAE42', // yellow orange
+    ice: '#ADD8E6', // light blue
+    fighting: '#A52A2A', // brown
+    poison: '#800080', // purple
+    ground: '#D2B48C', // tan
+    flying: '#B0E0E6', // light sky blue
+    psychic: '#FF69B4', // hot pink
+    bug: '#9ACD32', // yellow green
+    rock: '#808080', // gray
+    ghost: '#4B0082', // indigo
+    dragon: '#000080', // navy
+    dark: '#000000', // black
+    steel: '#C0C0C0', // silver
+    fairy: '#FFC0CB', // pink
+  }
+  return colors[type] ? `${colors[type]}` : '#808080'
+}
