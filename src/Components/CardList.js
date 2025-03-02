f03a69e5ad12d00dd7c4f5fb690bd7012e09f2fd
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-03-02 08:30:57 +0700

    Change props label

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import Cards from './Cards'
 
-const CardList = ({ pokemons }) => {
+const CardList = ({ data }) => {
   return (
-    <div className='flex justify-around flex-wrap my-6 lg:mx-20 md:mx-10 mx-2'>
-      {pokemons.map((pokemon, i) => {
-        return (
-          <div className='lg:w-1/4 md:w-1/3 w-80 p-2' key={i}>
-            <Cards
-              image={pokemon.image}
-              id={pokemon.id}
-              name={pokemon.name}
-              color={pokemon.color}
-              stats={pokemon.stats}
-              type={pokemon.types}
-            />
-          </div>
-        )
-      })}
+    <div className='flex justify-around flex-wrap'>
+      {data.length > 0 &&
+        data.map((pokemon, i) => {
+          return (
+            <div className='lg:w-1/4 md:w-1/3 w-80 p-2' key={i}>
+              <Cards
+                image={pokemon.image}
+                id={pokemon.id}
+                name={pokemon.name}
+                color={pokemon.color}
+                stats={pokemon.stats}
+                type={pokemon.types}
+              />
+            </div>
+          )
+        })}
+      {!data.length === 0 && <div>No pokemon found!</div>}
     </div>
   )
 }
