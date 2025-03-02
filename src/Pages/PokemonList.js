6b1dd637c0afe96a6fcf1560f69f46f43c8206eb
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-03-02 09:30:22 +0700

    Fix handle scroll, change redux state

--- src/Pages/PokemonList.js
+++ src/Pages/PokemonList.js
@@ -1,89 +1,85 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { getPokemon } from '../Store/Action/Pokemon'
-import { connect } from 'react-redux'
+// import { setSearchTerm, setSearchResults } from '../Store/Action/Filter'
 
 import CardList from '../Components/CardList'
 import Layout from '../Components/Layout'
+import Filters from '../Components/Filters'
+import Loading from '../Components/Loading'
 
-const PokemonList = (props) => {
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [offset, setOffset] = useState(0)
+const PokemonList = () => {
+  const dispatch = useDispatch()
+  const { pokemons, loading, searchTerm, searchResults, error, filterLoading } =
+    useSelector((state) => state.Pokemon)
+
   const limit = 20
+  const [data, setData] = useState([])
+  const [offset, setOffset] = useState(0)
 
   useEffect(() => {
-    fetchPokemon()
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
+    dispatch(getPokemon({ limit, offset }))
     // eslint-disable-next-line
-  }, [offset])
+  }, [dispatch, offset])
 
   useEffect(() => {
-    setData((prev) => {
-      let updatedData = [...prev, ...props.pokemons].reduce((acc, current) => {
-        if (!acc.find((item) => item.name === current.name)) {
-          acc.push(current)
-        }
-        return acc
-      }, [])
-      return updatedData
-    })
-    // eslint-disable-next-line
-  }, [props.pokemons])
-
-  const debounce = (func, delay) => {
-    let debounceTimer
-    return (...args) => {
-      clearTimeout(debounceTimer)
-      debounceTimer = setTimeout(() => func.apply(this, args), delay)
+    if (searchTerm.trim() === '') {
+      setData((prev) => {
+        let updatedData = [...prev, ...pokemons].reduce((acc, current) => {
+          if (!acc.find((item) => item.name === current.name)) {
+            acc.push(current)
+          }
+          return acc
+        }, [])
+        return updatedData
+      })
+    } else {
+      setData(searchResults)
     }
-  }
+  }, [searchTerm, pokemons, searchResults])
 
-  const handleScroll = debounce(() => {
+  const handleScroll = () => {
     const scrollPosition =
       window.innerHeight + document.documentElement.scrollTop
     const documentHeight = document.documentElement.offsetHeight
-    const margin = window.innerWidth < 640 ? 10 : 5 // Adjust margin for small screens
 
-    if (scrollPosition + margin >= documentHeight) {
+    if (scrollPosition >= documentHeight - 5 && !loading) {
       setOffset((prev) => prev + limit)
     }
-  }, 1500)
+  }
 
-  const fetchPokemon = async () => {
-    try {
-      setLoading(true)
-      await props.getPokemon({ limit, offset })
-    } catch (error) {
-      console.error('Error fetching pokemon', error)
-    } finally {
-      setLoading(false)
-    }
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+    // eslint-disable-next-line
+  }, [loading])
+
+  if (pokemons.length === 0 && loading) {
+    return (
+      <div className='flex justify-center items-center min-h-screen'>
+        <Loading size={'xl'} />
+      </div>
+    )
   }
 
   return (
     <Layout>
-      {data.length === 0 ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <h1 className='py-5 text-2xl font-bold'>Poké Card</h1>
-          <CardList pokemons={data} />
-          {!loading && (
-            <p className='pb-10 text-2xl'>Loading more pokemon...</p>
-          )}
-        </>
+      <h1 className='pt-5 text-2xl font-bold'>Pokédex</h1>
+      <Filters />
+      {error && <p className='mt-5'>{error}</p>}
+      <div className='lg:mx-20 md:mx-10 mx-2 my-6'>
+        <CardList data={data} />
+        {loading && (
+          <h2 className='text-xl text-center'>Load more pokemon ...</h2>
+        )}
+      </div>
+      {filterLoading && searchResults.length === 0 && (
+        <div className='mt-20'>
+          <Loading size={'md'} />
+        </div>
       )}
     </Layout>
   )
 }
 
-const mapStateToProps = (state) => {
-  return {
-    pokemons: state.Pokemon.pokemons,
-  }
-}
-
-export default connect(mapStateToProps, {
-  getPokemon,
-})(PokemonList)
+export default PokemonList
