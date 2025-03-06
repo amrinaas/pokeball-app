4caf9394098b2ed266b3db4cc303059dda90dd10
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-03-06 10:57:00 +0700

    Add new action type on reducer

--- src/Store/Reducer/Filter.js
+++ src/Store/Reducer/Filter.js
@@ -1,117 +1,86 @@
-import { FILTERED_POKEMON, LOAD_POKEMON, POKEMON_TYPES } from '../Types.js'
+import {
+  FILTERED_POKEMON,
+  POKEMON_TYPES,
+  UPDATE_FILTER,
+  FILTER_POKEMON_BEGIN,
+  FILTER_POKEMON_ERROR,
+  UPDATE_FILTER_OFFSET,
+  RESET_FILTERED_POKEMON,
+  FETCH_ERROR,
+} from '../Types.js'
 
 const initialState = {
   filtered_pokemon: [],
-  all_pokemon: [],
   search_term: '',
+  types: [],
   filters: {
-    types: [],
+    types: '',
+    nameOrId: '',
     colors: [],
     abilities: [],
     height: [],
     weight: [],
   },
+  filterLoading: false,
+  error: null,
+  filteredOffset: 0,
 }
 
 const filterReducer = (state = initialState, action) => {
   switch (action.type) {
-    // case FILTERED_POKEMON:
-    //   const { filters, all_pokemon } = state
-    //   const { types, colors, abilities, height, weight } = filters
-    //   const { type, value } = action.payload
-    //   let tempPokemon = [...all_pokemon]
-
-    //   console.log('state', state, 'type', type, 'value', value)
-
-    //   if (types.length > 0) {
-    //     tempPokemon = tempPokemon.filter((pokemon) => {
-    //       return types.every((type) => pokemon.types.includes(type))
-    //     })
-    //   }
-    //   if (colors.length > 0) {
-    //     tempPokemon = tempPokemon.filter((pokemon) => {
-    //       return colors.includes(pokemon.color)
-    //     })
-    //   }
-    //   if (abilities.length > 0) {
-    //     tempPokemon = tempPokemon.filter((pokemon) => {
-    //       return abilities.every((ability) =>
-    //         pokemon.abilities.includes(ability)
-    //       )
-    //     })
-    //   }
-    //   if (height.length > 0) {
-    //     tempPokemon = tempPokemon.filter((pokemon) => {
-    //       return height.includes(pokemon.height)
-    //     })
-    //   }
-    //   if (weight.length > 0) {
-    //     tempPokemon = tempPokemon.filter((pokemon) => {
-    //       return weight.includes(pokemon.weight)
-    //     })
-    //   }
-    //   return {
-    //     ...state,
-    //     filtered_pokemon: tempPokemon,
-    //     filters: {
-    //       ...state.filters,
-    //     },
-    //   }
-    case LOAD_POKEMON:
+    case FILTER_POKEMON_BEGIN:
       return {
         ...state,
-        all_pokemon: [...action.payload],
-        filtered_pokemon: [...action.payload],
-        filters: {
-          ...state.filters,
-        },
+        filterLoading: true,
+        error: null,
       }
     case FILTERED_POKEMON:
-      const { filters, all_pokemon } = state
-      const { type, value } = action.payload
-      const newFilters = { ...filters }
-      console.log(
-        'newFilters',
-        newFilters,
-        'type',
-        type,
-        'value',
-        value,
-        'all_pokemon',
-        all_pokemon
-      )
-
-      const newFilteredPokemon = all_pokemon.filter((pokemon) => {
-        if (type === 'types') {
-          return pokemon.types.includes(value)
-        }
-        if (type === 'colors') {
-          return pokemon.color === value
-        }
-        if (type === 'abilities') {
-          return pokemon.abilities.includes(value)
-        }
-        if (type === 'height') {
-          return pokemon.height === value
-        }
-        if (type === 'weight') {
-          return pokemon.weight === value
-        }
-        return pokemon
-      })
       return {
         ...state,
-        filtered_pokemon: newFilteredPokemon,
-        filters: newFilters,
+        filtered_pokemon: [...state.filtered_pokemon, ...action.payload],
+        filterLoading: false,
+        error: null,
+      }
+    case UPDATE_FILTER:
+      const { name, value } = action.payload
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          [name]: value,
+        },
+        filtered_pokemon: [],
+        filteredOffset: 0,
+        error: null,
+      }
+    case FILTER_POKEMON_ERROR:
+      return {
+        ...state,
+        filtered_pokemon: [],
+        filterLoading: false,
+        error: action.error,
       }
     case POKEMON_TYPES:
       return {
         ...state,
-        filters: {
-          ...state.filters,
-          types: action.payload,
-        },
+        types: action.payload,
         error: null,
+      }
+    case UPDATE_FILTER_OFFSET:
+      return {
+        ...state,
+        filteredOffset: action.payload,
+      }
+    case RESET_FILTERED_POKEMON:
+      return {
+        ...state,
+        filtered_pokemon: [],
+      }
+    case FETCH_ERROR:
+      return {
+        ...state,
+        filterLoading: false,
+        error: action.error,
       }
     default:
       break
