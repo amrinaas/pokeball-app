ba63d0ed66a1e9f866ca1b90b4b2071755d09a90
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-03-06 10:58:17 +0700

    Add filter by pokemon type

--- src/Components/Filters.js
+++ src/Components/Filters.js
@@ -1,52 +1,57 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Input from './Input'
 import Button from './Button'
 import { getPokemon } from '../Store/Action/Pokemon.js'
 import {
-  filterPokemon,
-  setSearchTerm as setSearchTermAction,
-  searchPokemon,
-  clearSearchTerm as clearSearchTermAction,
   pokemonType,
-  // filterPokemonByType,
+  filterPokemon,
+  resetFilteredPokemon,
 } from '../Store/Action/Filter.js'
 import Badge from './Badge.js'
-import { getColorBadgeHex } from '../utils.js'
-import { LOAD_POKEMON } from '../Store/Types.js'
+import { getColorBadgeHex, getUniqueValues } from '../utils.js'
+import { UPDATE_FILTER } from '../Store/Types.js'
 
 const Filters = () => {
   const dispatch = useDispatch()
-  const { searchTerm, pokemons } = useSelector((state) => state.Pokemon)
-  const { filtered_pokemon, filters } = useSelector((state) => state.Filter)
-  const [inputValue, setInputValue] = useState(searchTerm)
+  const { pokemons } = useSelector((state) => state.Pokemon)
+  const { filters, types } = useSelector((state) => state.Filter)
 
   useEffect(() => {
     dispatch(pokemonType())
-    dispatch({ type: LOAD_POKEMON, payload: pokemons })
     // eslint-disable-next-line
   }, [pokemons])
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value)
-  }
+  useEffect(() => {
+    dispatch(filterPokemon(filters))
+    // eslint-disable-next-line
+  }, [filters, dispatch])
 
-  const handleSearchClick = () => {
-    dispatch(setSearchTermAction(inputValue))
-    dispatch(searchPokemon(inputValue))
-  }
+  // const handleSearchClick = () => {
+  //   dispatch({ type: FILTERED_POKEMON })
+  // }
 
   const handleResetFilter = () => {
-    setInputValue('')
-    dispatch(clearSearchTermAction())
     dispatch(getPokemon({ limit: 20, offset: 0 }))
   }
 
-  const handleBadgeClick = (type) => {
-    // dispatch(filterPokemonByType(type))
-    dispatch(filterPokemon('types', type))
+  const handleUpdateFilter = (event) => {
+    let name = event.currentTarget.name
+    let value = event.target.value
+
+    if (name === 'types') {
+      value = event.target.innerText
+    }
+
+    if (name === 'nameOrId') {
+      value = event.target.value
+    }
+
+    dispatch(resetFilteredPokemon())
+    dispatch({ type: UPDATE_FILTER, payload: { name, value } })
   }
-  // console.log('filtered_pokemon', filtered_pokemon)
+
+  const pokemonTypes = getUniqueValues(types, 'name')
 
   return (
     <div className='px-10 text-left bg-black/60 p-5'>
@@ -54,26 +59,29 @@ const Filters = () => {
         <Input
           placeholder={'Search pokemon by name or pokemon ID'}
           label={'Search Pokemon'}
-          value={inputValue}
-          handleChange={handleInputChange}
+          value={filters.nameOrId}
+          handleChange={handleUpdateFilter}
           customStyle='w-3/4'
+          name={'nameOrId'}
         />
         <div className='flex ml-5'>
-          <Button handleClick={handleSearchClick}>Search</Button>
+          {/* <Button handleClick={handleSearchClick}>Search</Button> */}
           <Button handleClick={handleResetFilter}>Reset filter</Button>
         </div>
       </div>
       <div className='mt-5'>
         <h1>Filter by Type: </h1>
         <p className='flex flex-wrap'>
-          {filters.types.map((type) => (
-            <span
-              key={type.name}
+          {pokemonTypes.map((type) => (
+            <button
+              key={type}
+              name='types'
+              type='button'
+              onClick={handleUpdateFilter}
               className='leading-loose mr-0.5 ml-0.5 first:ml-0 hover:bg-gray-500 cursor-pointer'
-              onClick={() => handleBadgeClick(type.name)}
             >
-              <Badge text={type.name} color={getColorBadgeHex(type.name)} />
-            </span>
+              <Badge text={type} color={getColorBadgeHex(type)} />
+            </button>
           ))}
         </p>
       </div>
